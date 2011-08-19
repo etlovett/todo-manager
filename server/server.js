dbf35e742a639b7df85abd96befebc0a90c85542
Eric Lovett <etlovett@gmail.com>
2011-08-18 17:53:13 -0700

    implement normalizeTodos.  add basic http server to allow a client to get the todo list.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+var http = require("http");
 
 var todoFile = "./todo.txt";
 var todoFileOut = "./todo2.txt";
@@ -13,14 +14,31 @@ fs.readFile(todoFile, "utf8", function (err, data) {
 	var todoArray = deserializeTodos(data);
 	todoArray = normalizeTodos(todoArray);
 	
-	var todoString = serializeTodos(todoArray);
-	fs.writeFile(todoFileOut, todoString, "utf8", function (err) {
-		if (err) {
-			throw err;
+	http.createServer(function (request, response) {
+		if (request.method === "GET") {
+			response.writeHead(200, {
+				"content-type": "application/json"
+			});
+			response.write(JSON.stringify(todoArray));
+			response.end("\n");
+		} else if (request.method === "POST") {
+			response.end("sweet! \n");
+		} else {
+			response.statusCode = 405;
+			response.end("Only GET and POST are supported");
 		}
-		
-		console.log("written!");
+	}).listen(5834, function () {
+		console.log("listening!");
 	});
+	
+	// var todoString = serializeTodos(todoArray);
+	// fs.writeFile(todoFileOut, todoString, "utf8", function (err) {
+	// 	if (err) {
+	// 		throw err;
+	// 	}
+	// 	
+	// 	console.log("written!");
+	// });
 });
 
 
@@ -32,7 +50,7 @@ function deserializeTodos(fileString) {
 			return null;
 		}
 		
-		var match = todoString.match(/^([0-9]+)\.?\s*(.*)$/i);
+		var match = todoString.match(/^([0-9]+)[^a-zA-Z0-9]{0,2}\s*(.*)$/i);
 		
 		if (!todoString) {
 			return null;
@@ -42,11 +60,11 @@ function deserializeTodos(fileString) {
 			index: match[1],
 			todo: match[2]
 		};
-	});
+	}, this);
 	
 	todoArray = todoArray.filter(function (todoObj) {
 		return !!todoObj;
-	});
+	}, this);
 	
 	return todoArray;
 }
@@ -56,7 +74,13 @@ function normalizeTodos(todoArray) {
 		throw new Error("normalizeTodos: todoArray must be an array!");
 	}
 	
-	//TODO: this
+	todoArray = sortTodos(todoArray);
+	todoArray = todoArray.map(function (curTodo, index) {
+		return {
+			index: index + 1,
+			todo: curTodo.todo
+		}
+	}, this);
 	
 	return todoArray;
 }
@@ -66,13 +90,17 @@ function serializeTodos(todoArray) {
 		throw new Error("serializeTodos: todoArray must be an array!");
 	}
 	
-	todoArray = todoArray.sort(function (todo1, todo2) {
-		return (todo1.index - todo2.index);
-	});
+	todoArray = sortTodos(todoArray);
 	
 	var todoString = todoArray.reduce(function (acc, curTodo) {
 		return acc + curTodo.index + ". " + curTodo.todo + "\n";
 	}, "");
 	
 	return todoString;
+}
+
+function sortTodos(todoArray) {
+	return todoArray.sort(function (todo1, todo2) {
+		return (todo1.index - todo2.index);
+	});
 }
